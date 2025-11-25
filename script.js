1594d2533af4ccc24bab0195a67c66062eb359c7
Mirceaqq <mardarimircea830@gmail.com>
2025-11-25 08:37:32 +0200

    Am modificat java

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark');
+    document.body.classList.toggle('dark');
+});
+
+
+const scrollBtn = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 600) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+});
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Mesajul a fost trimis cu succes! Te vom contacta în curând.");
+    this.reset();
 });
